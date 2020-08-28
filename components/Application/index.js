4b237c8a36c11770e87cbe5314f836730f99774b
mashahori <masha.y@nimax.ru>
2020-08-28 19:07:26 +0300

    Fixies

--- components/Application/index.js
+++ components/Application/index.js
@@ -1,24 +1,18 @@
-import Button from 'elements/Button';
+import { Button } from 'elements';
 import * as S from './styles';
 
 const Application = ({ data, decor }) => {
-  const {
-    title,
-    text,
-    button,
-    button2,
-    picture
-  } = data;
+  const { title, text, button, button2, picture } = data;
   return (
     <S.Section>
       <S.Wrapper decor={decor}>
-        <S.H2 content={title} />
-        {text && <S.Text>{text}</S.Text>} 
+        <S.H2>{title}</S.H2>
+        {text && <S.Text>{text}</S.Text>}
         <S.Container>
           <S.StyledButton type="accent">{button}</S.StyledButton>
           {button2 && <Button>{button2}</Button>}
         </S.Container>
-        {picture && <S.Picture src={picture} alt='' />}
+        {picture && <S.Picture src={picture} alt={title} />}
       </S.Wrapper>
     </S.Section>
   );
