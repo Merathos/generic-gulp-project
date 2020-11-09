f579b01642ea666c50309a4a8651098864c8fb51
neolelya <neolelya@gmail.com>
2020-11-09 15:42:07 +0100

    1-main-fixes removed buttons with border from first slider, added fade effect

--- components/PortraitSlider/index.js
+++ components/PortraitSlider/index.js
@@ -1,79 +1,44 @@
 import Swiper from 'react-id-swiper';
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import ArrowPrev from 'public/icons/arrow-prev.svg';
 import ArrowNext from 'public/icons/arrow-next.svg';
 import * as S from './styles';
 
 const PortraitSlider = ({ pictures }) => {
   const ref = useRef(null);
-  const [activeSlide, setActiveSlide] = useState(1);
 
   const params = {
     loop: true,
+    effect: 'fade',
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
     renderPrevButton: () => {
       return (
-        <>
-          <S.PrevButtonMain
-            className="swiper-button-prev"
-            type="button"
-            aria-label="Previous slide"
-          >
-            <ArrowPrev />
-          </S.PrevButtonMain>
-          <S.PrevButton
-            className="swiper-button-prev"
-            type="button"
-            aria-label="Previous slide"
-          >
-            <ArrowPrev />
-          </S.PrevButton>
-        </>
+        <S.PrevButton
+          className="swiper-button-prev"
+          type="button"
+          aria-label="Previous slide"
+        >
+          <ArrowPrev />
+        </S.PrevButton>
       );
     },
     renderNextButton: () => (
-      <>
-        <S.NextButtonMain
-          className="swiper-button-next"
-          type="button"
-          aria-label="Next slide"
-        >
-          <ArrowNext />
-        </S.NextButtonMain>
-        <S.NextButton
-          className="swiper-button-next"
-          type="button"
-          aria-label="Next slide"
-        >
-          <ArrowNext />
-        </S.NextButton>
-      </>
+      <S.NextButton
+        className="swiper-button-next"
+        type="button"
+        aria-label="Next slide"
+      >
+        <ArrowNext />
+      </S.NextButton>
     ),
   };
 
   return (
-    <S.Section
-      className={activeSlide === 1 ? 'first-active-slide' : 'inactive-slide'}
-    >
-      <Swiper
-        ref={ref}
-        {...params}
-        on={{
-          slideChange: () => {
-            let activeIndex = ref.current.swiper.activeIndex;
-            if (activeIndex === 0) {
-              activeIndex = pictures.length;
-            }
-            if (activeIndex > pictures.length) {
-              activeIndex = 1;
-            }
-            setActiveSlide(activeIndex);
-          },
-        }}
-      >
+    <S.Section>
+      <Swiper ref={ref} {...params}>
         {pictures.map((el, i) => (
           <S.Element key={i} isMain={el.isMain}>
             <S.Img
