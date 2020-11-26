b3399678e37b3eca4ee8d31b2498b350ae586d44
dzyubanov <dzyubanov@htmlacademy.ru>
2020-11-26 10:51:22 +0300

    Change GreyFooter '#' to '' if no href

--- components/GreyFooter/index.js
+++ components/GreyFooter/index.js
@@ -18,7 +18,7 @@ const GreyFooter = ({ data, type, background }) => {
         <S.ListContainer background={background} type={type}>
           <S.List>
             {data.map((el, i) => (
-              <Link href={el.href ? el.href : '#'} key={i} passHref>
+              <Link href={el.href ? el.href : ''} key={i} passHref>
                 <S.Element>
                   <S.Title>{el.title}</S.Title>
                   <S.Text>{el.text}</S.Text>
