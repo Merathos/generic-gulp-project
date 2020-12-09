87ca5bbf7980a179afb63387773b3e83df9a1d4c
dzyubanov <dzyubanov@htmlacademy.ru>
2020-12-09 18:42:24 +0300

    Add redirect  for blog-list

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ module.exports = {
         destination: '/',
         permanent: true,
       },
+      {
+        source: '/blog-list',
+        destination: '/blog',
+        permanent: true,
+      },
     ];
   },
 };
