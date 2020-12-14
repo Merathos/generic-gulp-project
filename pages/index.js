b630c4f7cbebfe6b857bca5d97789f468eeb450b
dzyubanov <dzyubanov@htmlacademy.ru>
2020-12-14 13:09:58 +0300

    Update index for apollo 3

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { Layout, Main } from 'containers';
 import mock from 'mock/index';
 import { initializeApollo } from 'lib/apollo';
+import { useQuery } from '@apollo/client';
 
 import {
   GET_QUOTES,
@@ -10,17 +11,27 @@ import {
   GET_VACANCY_CATEGORIES_MAIN,
 } from 'graphql/main';
 
-const mainPage = ({ quotes, banner, blogs, teams, vacancy_categories }) => {
+const mainPage = () => {
+  const { data: quotesData } = useQuery(GET_QUOTES);
+  const { data: bannerData } = useQuery(GET_BANNER);
+  const { data: blogsData } = useQuery(GET_BLOGS_MAIN, {
+    variables: { is_home: true },
+  });
+  const { data: teamsData } = useQuery(GET_TEAMS_MAIN, {
+    variables: { is_home: true },
+  });
+  const { data: vacancyCatData } = useQuery(GET_VACANCY_CATEGORIES_MAIN);
+
   return (
     <Layout greyFooter plainHeader smallPadding isVisible={false}>
       <Main
         data={mock.main}
         storiesData={mock.stories}
-        quotes={quotes}
-        banner={banner}
-        blogs={blogs}
-        teams={teams}
-        vacancyCat={vacancy_categories}
+        quotes={quotesData}
+        banner={bannerData}
+        blogs={blogsData}
+        teams={teamsData}
+        vacancyCat={vacancyCatData}
       />
     </Layout>
   );
@@ -29,41 +40,27 @@ const mainPage = ({ quotes, banner, blogs, teams, vacancy_categories }) => {
 export async function getServerSideProps() {
   const apolloClient = initializeApollo();
 
-  const {
-    data: { quotes },
-  } = await apolloClient.query({
+  await apolloClient.query({
     query: GET_QUOTES,
   });
-  const {
-    data: { banner },
-  } = await apolloClient.query({
+  await apolloClient.query({
     query: GET_BANNER,
   });
-  const {
-    data: { blogs },
-  } = await apolloClient.query({
+  await apolloClient.query({
     query: GET_BLOGS_MAIN,
     variables: { is_home: true },
   });
-  const {
-    data: { teams },
-  } = await apolloClient.query({
+  await apolloClient.query({
     query: GET_TEAMS_MAIN,
     variables: { is_home: true },
   });
-  const {
-    data: { vacancy_categories },
-  } = await apolloClient.query({
+  await apolloClient.query({
     query: GET_VACANCY_CATEGORIES_MAIN,
   });
 
   return {
     props: {
-      quotes,
-      banner,
-      blogs,
-      teams,
-      vacancy_categories,
+      initialApolloState: apolloClient.cache.extract(),
     },
   };
 }
