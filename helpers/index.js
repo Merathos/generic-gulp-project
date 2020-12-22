0d99775a46cc59bf93657e863550c07fd9ff9043
neolelya <neolelya@gmail.com>
2020-12-22 17:28:13 +0100

    #228 About: added pluralize function and constant for company's lifetime

--- helpers/index.js
+++ helpers/index.js
@@ -9,7 +9,6 @@ export const fromSecToDuration = duration => {
   return `${min}:${sec}`;
 };
 
-
 export const getNewTags = (arr, slug) => {
   if (!arr) {
     return [slug];
@@ -32,4 +31,24 @@ export const getNewTags = (arr, slug) => {
 export const restOfDuration = duration => {
   const time = duration - 1;
   return time;
+};
+
+export const YEAR_OF_FOUNDATION = 1998;
+
+export const COMPANY_AGE = new Date().getFullYear() - YEAR_OF_FOUNDATION;
+
+export const pluralize = (number, one, two, five) => {
+  let n = Math.abs(number);
+  n %= 100;
+  if (n >= 5 && n <= 20) {
+    return five;
+  }
+  n %= 10;
+  if (n === 1) {
+    return one;
+  }
+  if (n >= 2 && n <= 4) {
+    return two;
+  }
+  return five;
 };
