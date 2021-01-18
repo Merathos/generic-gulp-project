9ab5103e69762f1a6c6ae37e8afbb99885dcad78
dzyubanov <dzyubanov@htmlacademy.ru>
2021-01-18 16:44:53 +0300

    Put title & tabs back

---
+++ forms/Tabs/index.js
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import * as S from './styles';
+
+const Tabs = ({ tabs }) => {
+  const { query } = useRouter();
+  return (
+    <S.Wrapper>
+      <S.List>
+        {tabs.map((el, i) => (
+          <S.Item key={i}>
+            <Link href={`/blog/${encodeURIComponent(el.slug)}`}>
+              <S.Label activeLink={query.slug === el.slug}>{el.title}</S.Label>
+            </Link>
+          </S.Item>
+        ))}
+      </S.List>
+    </S.Wrapper>
+  );
+};
+
+export default Tabs;
