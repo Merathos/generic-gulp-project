5989ddde2fdb50a81c40f8f494d573f23dce8f69
dzyubanov <dzyubanov@htmlacademy.ru>
2021-01-20 13:13:37 +0300

    Fix event speakers render

--- containers/speakers/index.js
+++ containers/speakers/index.js
@@ -9,11 +9,14 @@ const Speakers = props => {
       <S.Container>
         <S.Title>{speakersTitle}</S.Title>
         <S.List>
-          {speakers.map((el, i) => (
-            <S.Item key={i}>
-              <SpeakerCard data={el} />
-            </S.Item>
-          ))}
+          {speakers.map((el, i) => {
+            if (!el) return null;
+            return (
+              <S.Item key={i}>
+                <SpeakerCard data={el} />
+              </S.Item>
+            );
+          })}
         </S.List>
       </S.Container>
     </S.Section>
