230c5ca9da9bf7129fd334058998f3d92b48d1a9
dzyubanov <dzyubanov@htmlacademy.ru>
2021-03-16 16:47:01 +0300

    #379: Fix custom scrollbar & filter checkbox hover

--- pages/_app.js
+++ pages/_app.js
@@ -147,50 +147,6 @@ const GlobalStyle = createGlobalStyle`
     }
   }
 
-  .track-vertical {
-    position: absolute;
-    width: 3px !important;
-    right: 0;
-    bottom: 2px;
-    top: 2px;
-    border-radius: 6px;
-    background-color: #f7f8f9;
-    z-index: 10;
-  }
-
-  .thumb-vertical {
-    position: relative;
-    display: block;
-    width: 100%;
-    cursor: pointer;
-    border-radius: inherit;
-    background-color: #53b443 !important;
-    height: 84px;
-    transform: translateY(0px);
-  }
-
-  .scroll-view {
-    ::-webkit-scrollbar {
-      -webkit-appearance: none;
-    }
-  }
-
-  .scroll-view--scrolling {
-    &::-webkit-scrollbar {
-      width: 3px;
-    }
-
-    &::-webkit-scrollbar-track {
-      background: #F7F8F9;
-    }
-
-    &::-webkit-scrollbar-thumb {
-      background-color: #53B443;
-      border-radius: 6px;
-      border: 3px solid #53B443;
-    }
-  }
-
   .map-container {
     width: 100%;
     height: 100%;
