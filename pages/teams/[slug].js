d303c7f44129d129f2650bdfb87956b7af7fc0f6
Clients Nimax <dev@nimax.ru>
2021-05-15 19:09:32 +0300

    #386: Use one small size back button everywhere

--- pages/teams/[slug].js
+++ pages/teams/[slug].js
@@ -37,7 +37,7 @@ const ProjectPage = () => {
           <meta name="robots" content="noindex, nofollow" />
         )}
       </Head>
-      <Layout backButton smallButton greyFooter plainHeader>
+      <Layout backButton greyFooter plainHeader>
         <Project data={mock.project} teams={team} />
       </Layout>
     </>
